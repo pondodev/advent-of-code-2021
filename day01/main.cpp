44be37608d85914cb606786b10102e0c11842de3
Daniel Coady <me@pondo.dev>
2021-12-01 17:51:24 +1100

    day 1 part 2

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 const std::string INPUT_FILE_NAME = "input.txt";
 
+void part1();
+void part2();
+
 int main() {
+    part1();
+    part2();
+
+    return 0;
+}
+
+void part1() {
     std::ifstream f;
     f.open( INPUT_FILE_NAME );
 
@@ -21,10 +33,32 @@ int main() {
         last_val = val;
     }
 
-    printf("count: %d\n", count);
+    printf( "part 1 count: %d\n", count );
 
     f.close();
-
-    return 0;
 }
 
+void part2() {
+    std::ifstream f;
+    f.open( INPUT_FILE_NAME );
+
+    std::vector<int> values;
+    std::string line;
+    size_t count = 0;
+
+    while ( std::getline( f, line ) )
+        values.push_back( std::stoi( line ) );
+
+    auto last_sum = values[ 0 ] + values[ 1 ] + values[ 2 ];
+
+    for ( size_t i = 1; i < values.size() - 2; ++i ) {
+        auto sum = values[ i ] + values[ i + 1 ] + values[ i + 2 ];
+
+        if ( sum > last_sum ) ++count;
+        last_sum = sum;
+    }
+
+    printf( "part 2 count: %d\n", count );
+
+    f.close();
+}
