9cc811a9713a190be39027192faae3784e450922
Daniel Coady <me@pondo.dev>
2021-12-01 18:30:06 +1100

    refactor day 1, add helpers

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,3 +1,5 @@
+#include "helpers.hpp"
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -17,39 +19,27 @@ int main() {
 }
 
 void part1() {
-    std::ifstream f;
-    f.open( INPUT_FILE_NAME );
+    std::vector<int> values = Helpers::parse_file<int>( INPUT_FILE_NAME,
+        [](std::string line) {
+            return std::stoi( line );
+        } );
 
-    std::string line;
     size_t count = 0;
 
-    std::getline( f, line );
-    auto last_val = std::stoi( line );
-
-    while ( std::getline( f, line ) ) {
-        auto val = std::stoi( line );
-
-        if ( val > last_val ) ++count;
-        last_val = val;
-    }
+    for ( size_t i = 1; i < values.size(); ++i )
+        if ( values[ i ] > values[ i - 1 ] ) ++count;
 
     printf( "part 1 count: %d\n", count );
-
-    f.close();
 }
 
 void part2() {
-    std::ifstream f;
-    f.open( INPUT_FILE_NAME );
-
-    std::vector<int> values;
-    std::string line;
-    size_t count = 0;
-
-    while ( std::getline( f, line ) )
-        values.push_back( std::stoi( line ) );
+    std::vector<int> values = Helpers::parse_file<int>( INPUT_FILE_NAME,
+        [](std::string line) {
+            return std::stoi( line );
+        } );
 
     auto last_sum = values[ 0 ] + values[ 1 ] + values[ 2 ];
+    size_t count = 0;
 
     for ( size_t i = 1; i < values.size() - 2; ++i ) {
         auto sum = values[ i ] + values[ i + 1 ] + values[ i + 2 ];
@@ -59,6 +49,4 @@ void part2() {
     }
 
     printf( "part 2 count: %d\n", count );
-
-    f.close();
 }
